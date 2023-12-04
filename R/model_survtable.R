64e09e5040d90461d5f64fbac90fecd9b66c0742
jkoskenniemi <122159221+jkoskenniemi@users.noreply.github.com>
2023-12-04 15:24:38 -0500

    corrected a bug in model_survtable() that submodels were always the same as the full model

--- R/model_survtable.R
+++ R/model_survtable.R
@@ -29,23 +29,28 @@ model_survtable <- function(survtable) {
   
     if(submodels_requested == FALSE) {
       survtable <- dplyr::mutate(survtable, submodel_var = NA, submodel_value = NA)
-        }
-
-  model_list <- purrr::pmap(list(survtable$data_name, survtable$formula, survtable$exposure_var, survtable$outcome_var,
+    }
+    
+    model_list <- purrr::pmap(list(survtable$data_name, survtable$formula, survtable$exposure_var, survtable$outcome_var,
                                  survtable$submodel_var, survtable$submodel_value),
-                            function(data, formula, exposure_var, outcome_var, submodel_var, submodel_value) {
+                            function(data_name, formula, exposure_var, outcome_var, submodel_var, submodel_value) {
+                              
                               # Construct model names for each model
                               model_name <- ifelse(submodels_requested, 
                                                    paste0(outcome_var, "~", exposure_var, "|", 
-                                                          "filter(", data, ",", submodel_var, "==", submodel_value, ")"),
-                                                   paste0(outcome_var, "~", exposure_var, "|", data))
+                                                          "filter(", data_name, ",", submodel_var, "==", submodel_value, ")"),
+                                                   paste0(outcome_var, "~", exposure_var, "|", data_name))
                               
+
                               # Prepare the data frame
-                              data_df <- get(data)
+                              if(!is.na(submodel_var)) {
+                                filter_expr <- rlang::expr(dplyr::filter(!!rlang::sym(data_name), !!rlang::sym(submodel_var) == !!submodel_value))
+                                data_df <- eval(filter_expr)
+                              }  else if(is.na(submodel_var)) data_df <- get(data_name)
+                              
                               
                               # Construct and evaluate the formula
                               cox_formula <- as.formula(formula)
-                              
                               # Run cox ph models
                               # model <- analyze_coxph(data_df, cox_formula)
                               model <- survival::coxph(formula = cox_formula, data = data_df)
@@ -54,7 +59,7 @@ model_survtable <- function(survtable) {
                               attr(model, "model_name")    <- model_name
                               attr(model, "exposure")      <- exposure_var
                               attr(model, "outcome")       <- outcome_var
-                              attr(model, "data")          <- data
+                              attr(model, "data")          <- data_name
                               attr(model, "submodel_var")  <- submodel_var
                               attr(model, "submodel_value") <- submodel_value
                               
