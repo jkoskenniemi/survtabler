169c40a71a859ecb3026cf4a127e2c19d8dfa48d
Jaakko Koskenniemi <jajoko@utu.fi>
2024-06-26 10:59:00 -0400

    added declaration of global variables

--- R/utils.R
+++ R/utils.R
@@ -15,3 +15,8 @@ ignore_unused_imports <- function() {
 remove_multiple_spaces <- function(text) {
   gsub(" {2,}", " ", text)
 }
+
+utils::globalVariables(c("error", "outcome_var", "exposure_var", "covariate_var",
+                         "term", "p.value", "sig", "outcome", "term" ,"estimate", 
+                         "std.error", "position_dodge", "element_text", "element_rect", "element_blank",
+                         "p", "sig", "submodel_value")) 
